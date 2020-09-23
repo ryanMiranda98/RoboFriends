7b57f0c7c21c8580c5eb7f95efb3804c5fc53a70
Ryan <ryanjude.miranda@gmail.com>
2020-09-23 20:20:24 +0300

    Updates

---
+++ precache-manifest.a80ae1f618259bd47853d035253c2b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ab37aa5025e3488dd71dc4bde0ec5a",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ad792d4ed8a05e5179d5",
+    "url": "/RoboFriends/static/css/main.2b313b55.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad792d4ed8a05e5179d5",
+    "url": "/RoboFriends/static/js/main.9920e051.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
