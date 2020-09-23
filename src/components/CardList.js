27edd1a607e050e6e9ba3b781e100a315e9b7060
Ryan <ryanjude.miranda@gmail.com>
2020-09-23 20:06:16 +0300

    init robofriends

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  if (true) throw new Error("Nooooo");
+  return (
+    <div>
+      {robots.map((robot) => (
+        <Card
+          key={robot.id}
+          id={robot.id}
+          name={robot.name}
+          email={robot.email}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,37 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Bret',
+        username: 'AE-K-12',
+        email: 'aek@space.com'
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        username: 'AE-K-13',
+        email: 'aek@space.com'
+    },
+    {
+        id: 3,
+        name: 'Jeff',
+        username: 'AE-K-14',
+        email: 'aek@space.com'
+    },
+    {
+        id: 4,
+        name: 'Allison',
+        username: 'AE-K-15',
+        email: 'aek@space.com'
+    },
+    {
+        id: 5,
+        name: 'Timmy',
+        username: 'AE-K-16',
+        email: 'aek@space.com'
+    },    {
+        id: 6,
+        name: 'Joe',
+        username: 'AE-K-17',
+        email: 'aek@space.com'
+    }
+]
